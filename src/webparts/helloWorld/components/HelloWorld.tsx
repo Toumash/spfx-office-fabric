f11451ed687ca742fa169a13f10ac0babfa70869
Tomasz Dłuski <mr.toumash@gmail.com>
2019-03-16 01:44:55 +0100

    demo panel

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -3,30 +3,10 @@ import styles from "./HelloWorld.module.scss";
 import { IHelloWorldProps } from "./IHelloWorldProps";
 import { escape } from "@microsoft/sp-lodash-subset";
 import { DefaultButton } from "office-ui-fabric-react";
+import { PanelSmallRightExample } from "./PanelSmallRightExample";
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, {}> {
   public render(): React.ReactElement<IHelloWorldProps> {
-    return (
-      <div className={styles.helloWorld}>
-        <div className={styles.container}>
-          <div className={styles.row}>
-            <div className={styles.column}>
-              <span className={styles.title}>Welcome to SharePoint!</span>
-              <p className={styles.subTitle}>
-                Customize SharePoint experiences using Web Parts.
-              </p>
-              asdasdas
-              <DefaultButton>I am a button.</DefaultButton>
-              <p className={styles.description}>
-                {escape(this.props.description)}
-              </p>
-              <a href="https://aka.ms/spfx" className={styles.button}>
-                <span className={styles.label}>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    return <PanelSmallRightExample />;
   }
 }
